af154cc1dee26f33e11c34b97e2f63ba52b95836
abhishek-vamja <abhishekvamja2518@gmail.com>
2026-08-04 16:14:07 +0530

    Add Alembic configuration and initial migration scripts for task management

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,9 +1,35 @@
 """SQLAlchemy database model for Task entity."""
+from enum import Enum
 
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Enum as SqlEnum
 from apps.database import Base
 
+
+class TaskStatus(str, Enum):
+    """
+    Enumeration of all possible task statuses.
+
+    This enum is used to restrict the status field of a task
+    to a predefined set of valid values.
+    """
+
+    TODO = "todo"
+    IN_PROGRESS = "in_progress"
+    IN_REVIEW = "in_review"
+    DONE = "done"
+    
+class TaskType(str, Enum):
+    """
+    Enumeration of all possible task statuses.
+
+    This enum is used to restrict the status field of a task
+    to a predefined set of valid values.
+    """
+
+    FEATURE = "feature"
+    BUG = "bug"
+    TASK = "task"
 
 class Task(Base):
     """Task database model table 'tasks' bound to a foreign key User."""
@@ -16,3 +42,13 @@ class Task(Base):
     title: Mapped[str]
     description: Mapped[str | None]
     completed: Mapped[bool] = mapped_column(default=False)
+    status: Mapped[TaskStatus] = mapped_column(
+        SqlEnum(TaskStatus),
+        default=TaskStatus.TODO,
+        nullable=True,
+    )
+    type: Mapped[TaskType] = mapped_column(
+        SqlEnum(TaskType),
+        default=TaskType.TASK,
+        nullable=True,
+    )
